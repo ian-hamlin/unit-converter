dde01c805ed2bc367e37bc7a23453538f06489ca
ian-hamlin <ian@ihamlin.co.uk>
2020-08-02 17:37:51 +0100

    Updates

---
+++ precache-manifest.b42e1a976251376543ee5776d39fe9fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1585619b854b26d0187f39db8e3d7db9",
+    "url": "/unit-converter/index.html"
+  },
+  {
+    "revision": "38da963f4c9d17703006",
+    "url": "/unit-converter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "554977779ec44eef79f2",
+    "url": "/unit-converter/static/js/2.fe3406ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unit-converter/static/js/2.fe3406ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38da963f4c9d17703006",
+    "url": "/unit-converter/static/js/main.f2df7692.chunk.js"
+  },
+  {
+    "revision": "94fe0f52a951896eac75",
+    "url": "/unit-converter/static/js/runtime-main.e563c29f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/unit-converter/static/media/logo.5d5d9eef.svg"
+  }
+]);
