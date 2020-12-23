8c6b15319f0d48d8e683033e506f590773c3a730
ian-hamlin <ian@ihamlin.co.uk>
2020-12-23 12:54:03 +0000

    translation switch

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
@@ -6,8 +7,12 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import CategoryDisplay from "../CategoryDisplay/CategoryDisplay";
 import ConversionDisplay from "../ConversionDisplay/ConversionDisplay";
 
+
 const App = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  }
 
   return (
     <Container className="p-3">
@@ -40,6 +45,10 @@ const App = () => {
       </Router>
       <footer className="pt-5 text-muted text-center text-small">
         <p className="mb-1">icons by <a target="_blank" rel="noopener noreferrer" href="https://icons8.com">Icons8</a>, source code on <a target="_blank" rel="noopener noreferrer" href="https://github.com/ian-hamlin/unit-converter">GitHub</a></p>
+        <p>
+          <Button onClick={() => changeLanguage("en")}variant="link">{t("Lang_English")}</Button>
+          <Button onClick={() => changeLanguage("es")} variant="link">{t("Lang_Spanish")}</Button>
+        </p>
       </footer>
     </Container>
   );
